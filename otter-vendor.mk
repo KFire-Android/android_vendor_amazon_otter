cd19f904957170e557272fbaa9bb04ca01b8bc55
IngCr3at1on <sblood86@gmail.com>
2012-05-27 22:07:36 -0500

    Fix up blaze to otter changes and finish/fix setup_vendor script

--- otter-vendor.mk
+++ otter-vendor.mk
@@ -27,6 +27,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/amazon/blaze/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/amazon/otter/overlay
 
-$(call inherit-product, vendor/amazon/blaze/blaze-vendor-blobs.mk)
+$(call inherit-product, vendor/amazon/otter/otter-vendor-blobs.mk)
