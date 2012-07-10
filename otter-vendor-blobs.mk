150f9fc19121679d731c1ba5434867220557f2e0
Hashcode <hashcode0f@gmail.com>
2012-07-10 01:21:21 -0700

    vendor update
    Change-Id: I3ce5a8de762ea35de66ab7417e8c37e8206bf4c2

--- otter-vendor-blobs.mk
+++ otter-vendor-blobs.mk
@@ -17,45 +17,16 @@
 #
 # All the blobs necessary for Kindle Fire
 
-# TI-Connectivity / 127x .ini file
-PRODUCT_COPY_FILES += \
-    vendor/amazon/otter/proprietary/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
-    vendor/amazon/otter/proprietary/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
-    vendor/amazon/otter/proprietary/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    vendor/amazon/otter/proprietary/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin.orig \
-    vendor/amazon/otter/proprietary/etc/wifi/TQS_S_2.6.ini:system/etc/wifi/TQS_S_2.6.ini \
 
 # Prebuilts /bin
 PRODUCT_COPY_FILES += \
-    vendor/amazon/otter/proprietary/bin/rild:/system/bin/rild \
-    vendor/amazon/otter/proprietary/bin/strace:/system/bin/strace \
-    vendor/amazon/otter/proprietary/bin/temperature_log.sh:/system/bin/temperature_log.sh \
     vendor/amazon/otter/proprietary/bin/battery_log.sh:/system/bin/battery_log.sh \
-    vendor/amazon/otter/proprietary/bin/klog.sh:/system/bin/klog.sh \
-    vendor/amazon/otter/proprietary/bin/fix-mac.sh:/system/bin/fix-mac.sh \
     vendor/amazon/otter/proprietary/bin/idme:/system/bin/idme \
+    vendor/amazon/otter/proprietary/bin/klog.sh:/system/bin/klog.sh \
+    vendor/amazon/otter/proprietary/bin/temperature_log.sh:/system/bin/temperature_log.sh \
     vendor/amazon/otter/proprietary/bin/wpa_cli:/system/bin/wpa_cli \
     vendor/amazon/otter/proprietary/bin/wpa_supplicant:/system/bin/wpa_supplicant \
 
-# Prebuilts /system/etc
-PRODUCT_COPY_FILES += \
-    vendor/amazon/otter/proprietary/etc/gps.conf:/system/etc/gps.conf \
-    vendor/amazon/otter/proprietary/etc/media_profiles.xml:/system/etc/media_profiles.xml \
-    vendor/amazon/otter/proprietary/etc/vold.conf:/system/etc/vold.conf \
-    vendor/amazon/otter/proprietary/etc/vold.fstab:/system/etc/vold.fstab \
-    vendor/amazon/otter/proprietary/etc/firmware/ducati-m3.bin:/system/etc/firmware/ducati-m3.bin \
-
-#   vendor/amazon/otter/proprietary/usr/keychars/qtouch-touchscreen.kcm:/system/usr/keychars/qtouch-touchscreen.kcm \
-#   vendor/amazon/otter/proprietary/usr/keylayout/qtouch-touchscreen.kl:/system/usr/keylayout/qtouch-touchscreen.kl \
-#   vendor/amazon/otter/proprietary/usr/idc/omap-keypad.idc:/system/usr/idc/omap-keypad.idc \
-#   vendor/amazon/otter/proprietary/usr/keychars/omap-keypad.kcm:/system/usr/keychars/omap-keypad.kcm \
-#   vendor/amazon/otter/proprietary/usr/keylayout/omap-keypad.kl:/system/usr/keylayout/omap-keypad.kl \
-# Prebuilt /system/usr
-PRODUCT_COPY_FILES += \
-    vendor/amazon/otter/proprietary/usr/idc/ilitek_i2c.idc:/system/usr/idc/ilitek_i2c.idc \
-    vendor/amazon/otter/proprietary/usr/idc/twl6030_pwrbutton.idc:/system/usr/idc/twl6030_pwrbutton.idc \
-    vendor/amazon/otter/proprietary/usr/keylayout/twl6030_pwrbutton.kl:/system/usr/keylayout/twl6030_pwrbutton.kl \
-    vendor/amazon/otter/proprietary/usr/keylayout/AVRCP.kl:/system/usr/keylayout/AVRCP.kl \
 
 # Prebuilt /system/lib
 PRODUCT_COPY_FILES += \
