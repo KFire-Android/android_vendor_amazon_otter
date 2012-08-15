aeb919ea57c5ec696a81319c8825dfba42e7cbef
Hashcode <hashcode0f@gmail.com>
2012-08-14 21:49:41 -0700

    fix typo
    Change-Id: I35b7a1742b56bb7233a7974bc70e661ea70ffc69

--- otter-vendor-prebuilt.mk
+++ otter-vendor-prebuilt.mk
@@ -21,24 +21,24 @@ VENDOR_EXTRA := vendor/amzon/otter/proprietary
 
 # Prebuilts for wla
 PRODUCT_COPY_FILES += \
-    $(VENDOR_EXRTA)/bin/wpa_client:/system/bin/wpa_client \
-    $(VENDOR_EXRTA)/bin/wpa_client:/system/bin/wpa_supplicant \
-    $(VENDOR_EXRTA)/lib/libwpa_client.so:system/lib/libwpa_client.so \
-    $(VENDOR_EXRTA)/firmware/wlan/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
-    $(VENDOR_EXRTA)/firmware/wlan/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
-    $(VENDOR_EXRTA)/firmware/wlan/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    $(VENDOR_EXRTA)/firmware/wlan/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin \
+    $(VENDOR_EXTRA)/bin/wpa_client:/system/bin/wpa_client \
+    $(VENDOR_EXTRA)/bin/wpa_client:/system/bin/wpa_supplicant \
+    $(VENDOR_EXTRA)/lib/libwpa_client.so:system/lib/libwpa_client.so \
+    $(VENDOR_EXTRA)/firmware/wlan/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
+    $(VENDOR_EXTRA)/firmware/wlan/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
+    $(VENDOR_EXTRA)/firmware/wlan/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
+    $(VENDOR_EXTRA)/firmware/wlan/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin \
 
 # Prebuilt omap4xxx files
 PRODUCT_COPY_FILES += \
-    $(VENDOR_EXRTA)/omap4xxx/hwcomposer.otter.so:system/vendor/lib/hw/hwcomposer.otter.so \
-    $(VENDOR_EXRTA)/omap4xxx/libdomx.so:system/lib/libdomx.so \
-    $(VENDOR_EXRTA)/omap4xxx/libion.so:system/lib/libion.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.MISC.SAMPLE.so:system/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.CAMERA.so:system/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so:system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.DECODER.so:system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.H264E.so:system/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so:system/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so \
-    $(VENDOR_EXRTA)/omap4xxx/libOMX_Core.so:system/lib/libOMX_Core.so \
+    $(VENDOR_EXTRA)/omap4xxx/hwcomposer.otter.so:system/vendor/lib/hw/hwcomposer.otter.so \
+    $(VENDOR_EXTRA)/omap4xxx/libdomx.so:system/lib/libdomx.so \
+    $(VENDOR_EXTRA)/omap4xxx/libion.so:system/lib/libion.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.MISC.SAMPLE.so:system/lib/libOMX.TI.DUCATI1.MISC.SAMPLE.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.CAMERA.so:system/lib/libOMX.TI.DUCATI1.VIDEO.CAMERA.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so:system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.secure.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.DECODER.so:system/lib/libOMX.TI.DUCATI1.VIDEO.DECODER.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.H264E.so:system/lib/libOMX.TI.DUCATI1.VIDEO.H264E.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so:system/lib/libOMX.TI.DUCATI1.VIDEO.MPEG4E.so \
+    $(VENDOR_EXTRA)/omap4xxx/libOMX_Core.so:system/lib/libOMX_Core.so \
 
